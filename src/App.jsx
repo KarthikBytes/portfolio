5a8eba5f33a202ac6ef2099a1878e2788ea52af5
karthikbytes <karthikwork19@gmail.com>
2025-09-14 22:22:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './section/navbar'
+import Hero from './section/Hero';
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+    <Navbar />
+    <Hero />
+    {/*about*/}
+    {/*projects*/}
+    {/*experience*/}
+    {/*test*/}
+    {/*contact*/}
+    {/*footer*/}
+  </div>
+  );
+};
+export default App;
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,20 @@
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+      {/* Desktop */}
+      <div className="flex-col hidden md:flex c-space">
+        <h1 className='text-4xl font-medium'>Hi,I'm Karthik</h1>
+        <div className='flex flex-col items-start'>
+        <p className='text-5xl font-medium text-neutral-300'>
+             A developer<br />loves to create something 
+        </p>
+      </div>
+      </div>
+      {/* Mobile */}
+      
+      </div>
+    
+  );
+};
+
+export default HeroText;
