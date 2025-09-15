28916da9b6939e439aaad47a95716ef34fba362a
karthikbytes <karthikwork19@gmail.com>
2025-09-15 22:21:33 +0530

    Added responsive Hero section for desktop and mobile

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,19 +1,42 @@
+import { FlipWords } from "./Flipword";
+
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
       {/* Desktop */}
-      <div className="flex-col hidden md:flex c-space">
-        <h1 className='text-4xl font-medium'>Hi,I'm Karthik</h1>
-        <div className='flex flex-col items-start'>
-        <p className='text-5xl font-medium text-neutral-300'>
-             A developer<br />loves to create something 
-        </p>
+      <div className="hidden md:flex flex-col c-space">
+        <h1 className="text-4xl font-medium">Hi, I'm Karthik</h1>
+        <div className="flex flex-col items-start">
+          <p className="text-5xl font-medium text-neutral-300">
+            A developer<br />loves to create something
+          </p>
+          <div>
+            <FlipWords
+              words={["Code", "Build", "Deploy"]}
+              className="font-black text-white text-3xl"
+            />
+          </div>
+          <p className="text-4xl font-medium text-neutral-300">Web Solution</p>
+        </div>
       </div>
-      </div>
+
       {/* Mobile */}
-      
+      <div className="flex flex-col md:hidden c-space">
+        <h1 className="text-3xl font-medium">Hi, I'm Karthik</h1>
+        <div className="flex flex-col items-center">
+          <p className="text-4xl font-medium text-neutral-300">
+            A developer<br />loves to create something
+          </p>
+          <div>
+            <FlipWords
+              words={["Code", "Build", "Deploy"]}
+              className="font-black text-white text-2xl"
+            />
+          </div>
+          <p className="text-3xl font-medium text-neutral-300">Web Solution</p>
+        </div>
       </div>
-    
+    </div>
   );
 };
 
